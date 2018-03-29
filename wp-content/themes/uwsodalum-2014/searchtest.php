a189a95f907b8f6144fc00ba8b1d7052b7afdea7
anyalynn <anyal@uw.edu>
2018-03-29 14:46:19 -0700

    First Commit

---
+++ wp-content/themes/uwsodalum-2014/searchtest.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<body>
+
+
+<script>
+  (function() {
+    var cx = '013161175624235233741:myuhwwtboeq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+
