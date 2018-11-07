834d1d1aa043c419705a73af3f1673ef810a5cdb
ramonqu <ramonqu@uw.edu>
2018-11-07 10:34:04 -0800

    update all uwsodalum2018 files

--- wp-content/themes/uwsodalum-2018/inc/template-functions.php
+++ wp-content/themes/uwsodalum-2018/inc/template-functions.php
@@ -201,8 +201,8 @@ if ( ! function_exists('get_uw_breadcrumbs') ) :
 
     global $post;
     $ancestors = array_reverse( get_post_ancestors( $post->ID ) );
-    $html = '<li><a href="http://uw.edu" title="University of Washington">Home</a></li>';
-    $html .= '<li' . (is_front_page() ? ' class="current"' : '') . '><a href="' . home_url('/') . '" title="' . get_bloginfo('title') . '">' . get_bloginfo('title') . '</a><li>';
+    $html = '<li><a href="http://sodalum.uw.edu" title="UW School of Dentistry Alumni">Home</a></li>';
+    #$html .= '<li' . (is_front_page() ? ' class="current"' : '') . '><a href="' . home_url('/') . '" title="' . get_bloginfo('title') . '">' . get_bloginfo('title') . '</a><li>';
 
     if ( is_404() )
     {
